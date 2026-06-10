2146f96cef976f006b465b4dfa6ff13eeb4b507f
Neetu Khatri <neeturk2@gmail.com>
2026-06-10 09:12:59 -0500

    today commit

--- tests/E2E/fE2E.spec.js
+++ tests/E2E/fE2E.spec.js
@@ -1,15 +1,15 @@
-import { test } from '../../fixture/ekart.js';
-// import cardData from '../../testdata/card_details.json
+// import { test } from '../../fixture/ekart.js';
+// // import cardData from '../../testdata/card_details.json
 
-test('End To End Purchase Flow', async ({ e2ePurchasePage }) => {
+// test('End To End Purchase Flow', async ({ e2ePurchasePage }) => {
     
-    await e2ePurchasePage.launch();
-    await e2ePurchasePage.registerUser(email);
-    await e2ePurchasePage.searchProduct();
-    await e2ePurchasePage.addProductsToCart();
-    await e2ePurchasePage.checkout();
-    await e2ePurchasePage.enterPayment(cardData);
-    await e2ePurchasePage.verifyOrderSuccess();
-    await e2ePurchasePage.logoutUser();
+//     await e2ePurchasePage.launch();
+//     await e2ePurchasePage.registerUser(email);
+//     await e2ePurchasePage.searchProduct();
+//     await e2ePurchasePage.addProductsToCart();
+//     await e2ePurchasePage.checkout();
+//     await e2ePurchasePage.enterPayment(cardData);
+//     await e2ePurchasePage.verifyOrderSuccess();
+//     await e2ePurchasePage.logoutUser();
 
-});
+// });

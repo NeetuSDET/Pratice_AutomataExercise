fef3473fcd8f91c145833fcec93293bdf01fe118
Neetu Khatri <neeturk2@gmail.com>
2026-06-16 09:10:54 -0500

    commit

--- tests/Products/ProductDetails.spec.js
+++ tests/Products/ProductDetails.spec.js
@@ -3,11 +3,17 @@ import { test, expect } from '@playwright/test';
 test('Verify Product Details', async ({ page }) => {
   await page.goto('https://automationexercise.com/');
 
+  // await page.route('**/*google*/**', route => route.abort());
+  // await page.route('**/*adsbygoogle*/**', route => route.abort());
+
+  
+  // await page.locator('text=Close').click({ force: true });
+
   // Click Products (using force click here too just in case an ad blocks it)
   await page.locator('//a[@href="/products"]').click({ force: true });
 
   // Click first View Product using Playwright's native locator and a forced click
-  await page.getByRole('link', { name: 'View Product' }).first().click({ force: true });
+  await page.locator('(//a[text()="View Product"])[1]').click();
 
   // Define locators
   const productName = page.locator('//div[@class="product-information"]/h2');
